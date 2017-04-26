447f96bf395821670b321d46338bd53529b2e6b7
WIN_NT\x16j008 <x16j008@chiba-fjb.ac.jp>
2017-04-26 12:22:42 +0900

    カレンダーの作成

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateNow = DateTime.Now;
+            Console.Write("{0}年 {1}月\n", dateNow.Year, dateNow.Month);
+            Console.ReadLine();
+
+            DateTime dateFirst = DateTime.Parse(string.Format("{0}/{1}/1", dateNow.Year, dateNow.Month));
+            DisplayCalendar(dateFirst);
+            Console.ReadLine();
+        }
+        static void DisplayCalendar(DateTime dateFirst)
+        {
+            int week;
+            int month = dateFirst.Month;
+
+            Console.Write("--------------------\n");
+            Console.Write("日 月 火 水 木 金 土\n");
+
+            for (week = 0; week < (int)dateFirst.DayOfWeek; week++)
+                Console.Write("   ");
+
+            while (month == dateFirst.Month)
+            {
+                for (; week < 7 && month == dateFirst.Month; week++)
+                {
+                    Console.Write("{0,2} ", dateFirst.Day);
+                    dateFirst = dateFirst.AddDays(1);
+                }
+                Console.Write("\n");
+                week = 0;
+            }
+        }
+    }
+}
